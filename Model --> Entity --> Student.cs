df58c2422a133a7d729f869f9b19e3d8b9fa571a
Shweta Mulage <159747162+shweta-1512@users.noreply.github.com>
2025-08-01 16:50:09 +0530

    Create Model --> Entity --> Student.cs

---
+++ Model --> Entity --> Student.cs
@@ -0,0 +1,11 @@
+namespace SawggerDemo.Model
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string Course { get; set; } = string.Empty;
+    }
+}
